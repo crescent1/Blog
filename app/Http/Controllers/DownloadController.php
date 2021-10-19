dc24dbff194e743960f74e71736212e183822492
unknown <szuhri1@gmail.com>
2021-10-19 09:38:19 +0700

    perbaikan

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use PDF;
+// use Elibyy\TCPDF\Facades\TCPDF as PDF;
 use TCPDF as GlobalTCPDF;
+use Elibyy\TCPDF\Facades\TCPDF as PDF;
 
 class DownloadController extends Controller
 {
@@ -15,18 +16,27 @@ class DownloadController extends Controller
      */
     public function download()
     {
-        $pdf = new GlobalTCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-        $pdf->SetTitle('RSF-RAB');
-        $pdf->SetMargins(10, 5, 10);
-        $pdf->SetAutoPageBreak(true, 30);
+        // $pdf = new GlobalTCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        // $pdf->SetTitle('RSF-RAB');
+        // $pdf->SetMargins(10, 5, 10);
+        // $pdf->SetAutoPageBreak(true, 30);
 
-        // PDF::setHeaderCallback(function($pdf){
-        //     $pdf->SetFont('helvetica', 'B', 20);
-        //     // Title
-        //     $pdf->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
-        // });
 
-        $pdf->AddPage();
+
+        PDF::SetTitle('Hello World');
+        PDF::setHeaderCallback(function($pdf){
+            $pdf->SetFont('helvetica', 'B', 20);
+            // Title
+            $pdf->Cell(0, 30, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+        });
+        PDF::SetMargins(10, 5, 10);
+        PDF::AddPage();
+        PDF::Write(0, 'Hello World');
+
+        PDF::Output('hello_world.pdf');
+
+        // $pdf->AddPage();
+        // $pdf->Output(uniqid().'testing.pdf');
 
     }
 }
